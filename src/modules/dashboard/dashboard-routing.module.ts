ca6feba9d0c7ba64b74f1e2358a239cad65748cc
rishabh kalra <rishabh.kalra@niit.com>
2020-05-06 10:28:13 +0530

    default route to dashboard/home

--- src/modules/dashboard/dashboard-routing.module.ts
+++ src/modules/dashboard/dashboard-routing.module.ts
@@ -5,6 +5,11 @@ import { DashboardCoreComponent } from './components/dashboard-core/dashboard-co
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     component: DashboardCoreComponent,
   }
